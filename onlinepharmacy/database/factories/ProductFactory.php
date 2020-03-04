fca34d9d806c2cad511d6525a95c51481b2c41d7
janithaDassanayake <janithaprathapa@yahoo.com>
2020-03-04 15:19:46 +0530

    prouduct part started

---
+++ onlinepharmacy/database/factories/ProductFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\product;
+use Faker\Generator as Faker;
+
+$factory->define(product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->sentence(2),
+        'description' => $faker->sentence(10),
+        'price' => $faker->numberBetween(100,5000),
+        //
+    ];
+});
